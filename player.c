0f0c494f6c724a8b005ed69a39762a5de91339b0
kiwiryan <90732882+kiwiryan@users.noreply.github.com>
2021-12-17 22:17:00 -0500

    Create player.c

---
+++ player.c
@@ -0,0 +1,20 @@
+#include "player.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int health;
+int time;
+
+void setTime(int value)
+{}
+void setHealth(int value)
+{}
+int getHealth()
+{   
+    return health; 
+}
+int getTime()
+{
+    return time;
+}
